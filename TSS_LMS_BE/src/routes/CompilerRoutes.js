ce6a0c9ac70e4d4d004913fab9f59fdb1f513ce4
Patricia Torrez <aleptg10298@gmail.com>
2024-06-25 23:04:23 -0400

    Cambios-Compilador

--- TSS_LMS_BE/src/routes/CompilerRoutes.js
+++ TSS_LMS_BE/src/routes/CompilerRoutes.js
@@ -1,26 +1,31 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import fs from 'fs';
-import {exec} from 'child_process';
+import { exec } from 'child_process';
 import { v4 as uuidv4 } from 'uuid';
 
-const CompilerRoutes= Router();
-//toDo refactorizar
-CompilerRoutes.post('/compiler',async(req,res)=>{
-    const {className,code,extention}=req.body;
+const CompilerRoutes = Router();
 
-    const excecutionDirectory=`./assets/${uuidv4()}`;
-    const javaFileDir=`${excecutionDirectory}/${className}.${extention}`;
-    console.log(excecutionDirectory);
+CompilerRoutes.post('/compiler', async (req, res) => {
+    const { className, code, extention } = req.body;
+    const executionDirectory = `./assets/${uuidv4()}`;
+    const javaFileDir = `${executionDirectory}/${className}.${extention}`;
 
-    fs.mkdirSync(excecutionDirectory,{recursive:true});
+    try {
+        fs.mkdirSync(executionDirectory, { recursive: true });
 
-    fs.writeFile(javaFileDir, code, (err) => {if (err) throw err;}); 
+        fs.writeFileSync(javaFileDir, code);
 
-    exec(`javac ${javaFileDir} && java -cp ${excecutionDirectory} ${className}`, (err, stdout) => {if (err) {return;}
-    res.send(stdout)
-    });
-
+        exec(`javac ${javaFileDir} && java -cp ${executionDirectory} ${className}`, (err, stdout, stderr) => {
+            if (err) {
+                console.error(`Compilation or execution error: ${stderr}`);
+                return res.status(500).json({ error: stderr });
+            }
+            res.json({ output: stdout });
+        });
+    } catch (err) {
+        console.error(`Error handling file operations: ${err}`);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
-
 
 export default CompilerRoutes;
