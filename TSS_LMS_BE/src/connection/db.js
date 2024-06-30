070cd5c81ae29d133df38d7d3e2b3c77aeda30a3
IvanD-A <alarconivan73@gmail.com>
2024-06-30 15:29:55 -0400

    Update_CredencialesDB

--- TSS_LMS_BE/src/connection/db.js
+++ TSS_LMS_BE/src/connection/db.js
@@ -1,11 +1,11 @@
 import {createPool} from 'mysql2/promise'
 
 const connection=createPool({
-    host: 'monorail.proxy.rlwy.net',
+    host: 'viaduct.proxy.rlwy.net',
     user: 'root',
     database: 'railway',
-    port: 49646,
-    password: 'aAnsVlVxeKPbxarTLRfvUSgfoCqySUMr',
+    port: 20271,
+    password: 'ZJKzakRYAsiXFCIcbxQNmFdEpBIeyBKk',
 })
 
 export default connection;
