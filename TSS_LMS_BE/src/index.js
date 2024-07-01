94ddce8377cf55a2774b52f153831bb0cbaeaa34
IvanD-A <alarconivan73@gmail.com>
2024-07-01 01:16:48 -0400

    Cors2

--- TSS_LMS_BE/src/index.js
+++ TSS_LMS_BE/src/index.js
@@ -21,7 +21,7 @@ app.set("port",process.env.PORT || 3001);
 // Alternatively, configure specific options
 app.options('*', cors());
 app.use(cors({
-origin: 'http://localhost:3000', // React app URL
+origin: 'https://frontend-production-999e.up.railway.app', // React app URL
 methods: ['GET', 'POST','PUT', 'DELETE', 'OPTIONS'], // Allowed methods
 allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'access-control-allow-origin'] // Allowed headers
 }));
